5c5be38773004b607f8e58afdeef3d054a7770bd
Jorge F. Contreras <windblows3@yahoo.com>
2012-08-24 03:50:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rayuela69::Application.config.secret_token = 'e3cbceffe383b6db50cc73abea39a8116da10c130c66158b972b86c2301d81bbfd42ff783a02ec3a5f53a7a12d12115528b6006fc6f44ec0f151918cf7f37a56'
